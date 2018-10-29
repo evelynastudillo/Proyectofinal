8832c59073d6e7d5f02002688baaad1218ba25cb
evelyn <astudillobarra.evelyn@gmail.com>
2018-10-29 15:55:39 -0300

    Wip:  comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  #get 'comments/index'
+  #get 'comments/create'
+
   resources :posts
+  resources :comments
   devise_for :users
-  
+
   root to: 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
