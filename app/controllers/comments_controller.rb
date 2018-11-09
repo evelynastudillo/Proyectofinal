3d52de3edbc57b8d17e9e701571c3d0fa48eb26c
evelyn <astudillobarra.evelyn@gmail.com>
2018-11-08 21:21:31 -0300

    add:  identacion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
   end
@@ -9,17 +9,17 @@ before_action :authenticate_user!
   end
 
   def create
-  @post = Post.find(params[:post_id])
-  @comment = Comment.new(comment_params)
-  @comment.user_id = current_user.id
-  @post.comments << @comment
-  @comment.save!
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @post.comments << @comment
+    @comment.save!
 
-  #def show
-  #  @comment = Comment.find(params[:id])
-  #end
-  private def comment_params
-    params.require(:comment).permit(:comment, :user_id)
-   end
+    #def show
+    #  @comment = Comment.find(params[:id])
+    #end
+    private def comment_params
+      params.require(:comment).permit(:comment, :user_id)
+    end
   end
 end
