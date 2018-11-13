693c748599c7574a7709d07e4d26088623c8fb60
evelyn <astudillobarra.evelyn@gmail.com>
2018-11-13 19:50:02 -0300

    some changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,15 +7,15 @@ class CommentsController < ApplicationController
   end
 
   def create
-  @post = Post.find(params[:post_id])
-  @comment = @post.comment.new(comment_params)
-  @comment.save!
-
+    @post = Post.find(params[:post_id])
+    @comment = @post.comment.new(comment_params)
+    @comment.user = current_user
+    @comment.save!
+  end
   def show
     @comment = Comment.find(params[:id])
   end
   private def comment_params
     params.require(:comment).permit(:content)
-  end
   end
 end
