68623435d5a24301d6eca1e051fb3acbd7446c5e
evelyn <astudillobarra.evelyn@gmail.com>
2018-11-22 19:49:06 -0300

    wip comment functions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,15 +7,16 @@ class CommentsController < ApplicationController
   #end
 
   def create
-
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:comment))
-    @comment.user = current_user
+    @comment.user_id = current_user.id
     @comment.save!
   end
+
   def show
     @comment = Comment.find(params[:id])
   end
+
   private def comment_params
     params.require(:comment).permit(:content)
   end
