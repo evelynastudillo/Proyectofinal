b8083d59bb5d5d60ce91637299ac896a83202ea7
evelyn <astudillobarra.evelyn@gmail.com>
2018-11-28 12:26:19 -0300

    add method in models and controllers for omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,19 @@ class User < ApplicationRecord
 
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+  :recoverable, :rememberable, :validatable,
+  :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :comments
   has_many :posts
   has_many :likes, through: :posts, dependent: :destroy
 
 
-
-
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+    end
+  end  
 end
